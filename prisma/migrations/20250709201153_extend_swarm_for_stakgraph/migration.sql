beb44f2a6dc01f97ef9d953c0b27d8d9119b5970
Evanfeenstra <evanfeenstra@gmail.com>
2025-07-22 09:10:59 -0700

    clear migrations

--- prisma/migrations/20250709201153_extend_swarm_for_stakgraph/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "swarms" ADD COLUMN     "environment_variables" JSONB NOT NULL DEFAULT '[]',
-ADD COLUMN     "pool_name" TEXT,
-ADD COLUMN     "repository_description" TEXT,
-ADD COLUMN     "repository_name" TEXT,
-ADD COLUMN     "repository_url" TEXT,
-ADD COLUMN     "swarm_api_key" TEXT;
--- prisma/migrations/20250722143429_camel_case/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `sourceWebsocketId` on the `chat_messages` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "chat_messages" DROP COLUMN "sourceWebsocketId",
-ADD COLUMN     "source_websocket_id" TEXT;
