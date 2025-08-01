fefb28cf787d56f6219ea1d817724edba83a84aa
Gonzalo Javier Aune <gonzaloaune@gmail.com>
2025-08-01 15:18:03 +0100

    fixed repo2graphurl and escape chars

--- src/components/wizard/wizard-steps/review-pool-environment-step/index.tsx
+++ src/components/wizard/wizard-steps/review-pool-environment-step/index.tsx
@@ -146,23 +146,23 @@ services:
       - "localhost:172.17.0.1"
       - "host.docker.internal:host-gateway"
 `,
-    "Dockerfile": `FROM mcr.microsoft.com/devcontainers/universal
+    Dockerfile: `FROM mcr.microsoft.com/devcontainers/universal
 
 # [Optional] Uncomment this section to install additional OS packages.
-RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
+RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \\
     && apt-get -y install --no-install-recommends wget sed
 
-RUN sudo mkdir -p -m 755 /etc/apt/keyrings \
-    && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
-    && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-    && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-    && sudo apt update -y \
+RUN sudo mkdir -p -m 755 /etc/apt/keyrings \\
+    && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \\
+    && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \\
+    && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \\
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \\
+    && sudo apt update -y \\
     && sudo apt install gh -y
 
 # Install PM2 globally and ensure it's accessible
-RUN npm install -g pm2 && \
-    ln -sf /usr/local/node/bin/pm2 /usr/local/bin/pm2 && \
+RUN npm install -g pm2 && \\
+    ln -sf /usr/local/node/bin/pm2 /usr/local/bin/pm2 && \\
     pm2 --version
 `,
   };
