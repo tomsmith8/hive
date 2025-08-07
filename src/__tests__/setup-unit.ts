62cf24e94dbaf10eccaee60053bed52c41af932e
kelmith <faye.12222883@lpu.in>
2025-08-07 22:39:14 +0530

    fix env demands for test

--- src/__tests__/setup-unit.ts
+++ src/__tests__/setup-unit.ts
@@ -5,6 +5,9 @@ import { beforeAll, afterAll } from "vitest";
 // Add any global test setup here
 beforeAll(() => {
   // Setup any global test environment for unit tests
+  if (!process.env.TOKEN_ENCRYPTION_KEY) {
+    process.env.TOKEN_ENCRYPTION_KEY = "test-encryption-key-32-chars-long-here";
+  }
 });
 
 afterAll(() => {
