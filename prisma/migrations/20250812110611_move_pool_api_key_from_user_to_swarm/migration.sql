caa63e3e4740d3c0b60107837f5461aa00fe9c07
Tom Smith <142233216+tomsmith8@users.noreply.github.com>
2025-08-12 12:55:20 +0100

    fix: preserve pool_api_key in users table during migration
    Update migration to keep pool_api_key column in users table while adding
    it to swarms table. The users.pool_api_key will be removed in a separate PR.

--- prisma/migrations/20250812110611_move_pool_api_key_from_user_to_swarm/migration.sql
+++ prisma/migrations/20250812110611_move_pool_api_key_from_user_to_swarm/migration.sql
@@ -1,11 +1,2 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `pool_api_key` on the `users` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
 ALTER TABLE "swarms" ADD COLUMN     "pool_api_key" TEXT;
-
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "pool_api_key";
