2992a1504acf0cd803d203ea3178fb5856a09cfb
Evan Feenstra <evanfeenstra@gmail.com>
2025-08-29 11:07:46 -0700

    staklink-js dockerfile (#451)
    * staklink-js dockerfile
    
    * v0.1.1 staklink-js

--- src/utils/devContainerUtils.ts
+++ src/utils/devContainerUtils.ts
@@ -191,27 +191,7 @@ services:
 }
 
 export function dockerfileContent() {
-  return `FROM mcr.microsoft.com/devcontainers/universal
-
-# [Optional] Uncomment this section to install additional OS packages.
-RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \\
-    && apt-get -y install --no-install-recommends wget sed
-
-RUN sudo mkdir -p -m 755 /etc/apt/keyrings \\
-    && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \\
-    && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \\
-    && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \\
-    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \\
-    && sudo apt update -y \\
-    && sudo apt install gh -y
-
-# Install playwright
-RUN npx playwright install --with-deps
-
-# Install PM2 globally and ensure it's accessible
-RUN npm install -g pm2 && \\
-    ln -sf /usr/local/node/bin/pm2 /usr/local/bin/pm2 && \\
-    pm2 --version
+  return `FROM ghcr.io/stakwork/staklink-js:v0.1.1
 `;
 }
 
