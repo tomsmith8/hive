16be53ad4e2b074932026a421068066b3b720abf
Evan Feenstra <evanfeenstra@gmail.com>
2025-09-02 14:23:51 -0700

    separate next dev cache (#473)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  distDir: process.env.NODE_ENV === "development" ? ".next-dev" : ".next",
   output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
